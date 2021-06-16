f685855d7528cb2a00feab1e7f6debb4385ee193
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-16 18:35:38 +0200

    refactor: added bearer security to sheety

--- data_manager.py
+++ data_manager.py
@@ -5,15 +5,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SHEETY_PRICES_ENDPOINT = f"https://api.sheety.co/{os.getenv('SHEETY')}/flightDeals/prices"
+SHEETY_PRICES_ENDPOINT = f"https://api.sheety.co/{os.getenv('SHEETY_ID')}/flightDeals/prices"
 
 
 class DataManager:
     def __init__(self):
         self.destination_data = {}
+        self.bearer_headers = {"Authorization": f"Bearer {os.getenv('SHEETY_TOKEN')}"}
 
     def get_destination_data(self):
-        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, headers=self.bearer_headers)
         data = response.json()
         self.destination_data = data["prices"]
         return self.destination_data
@@ -21,5 +22,7 @@ class DataManager:
     def update_destination_codes(self):
         for city in self.destination_data:
             new_data = {"price": {"iataCode": city["iataCode"]}}
-            response = requests.put(url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}", json=new_data)
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}", json=new_data, headers=self.bearer_headers
+            )
             print(response.text)
